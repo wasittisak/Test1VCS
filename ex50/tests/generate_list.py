110ed65c17ecd4979869de55a8900ba28f794c5d
wasittisak <so.wasittisak_st@tni.ac.th>
2017-02-03 01:36:05 +0000

    First commit

---
+++ ex50/tests/generate_list.py
@@ -0,0 +1,17 @@
+import random
+def generate_list(): 
+     alist=[x for x in range(random.randint(-10,10))]
+     return alist
+     
+def printIt():
+    print(generate_list())
+    
+def main():
+    printIt()
+    
+if __name__ == 'main':
+    print("Test printIt():")
+    main()
+    
+    
+     

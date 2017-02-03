23a5d83dcb94b7acb4188d6170a7a506bd6008e6
wasittisak sookarom <so.wasittisak_st@tni.ac.th>
2017-02-03 08:49:04 +0700

    Update generate_list.py

--- ex50/tests/generate_list.py
+++ ex50/tests/generate_list.py
@@ -1,17 +1,14 @@
 import random
-def generate_list(): 
-     alist=[x for x in range(random.randint(-10,10))]
-     return alist
-     
+def generate_list():
+    alist = [x for x in range(random.randint(-10, 10))]
+    return alist
+
 def printIt():
     print(generate_list())
     
 def main():
     printIt()
     
-if __name__ == 'main':
-    print("Test printIt():")
+if __name__ == '__main__':
+    print("Test printIt() :")
     main()
-    
-    
-     
